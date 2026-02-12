c0c784a15bb7867febfec583a1361efb7f45a411
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2026-02-12 11:00:09 -0300

    update

---
+++ lavinia/alexandre/analise des. sistemas/aula_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+// biblioteca do tipo bool
+
+int main (){
+
+    int x = 10; // var tipo inteiro
+    float pi = 3.14; // var do tipo numeros decimais
+    char letra = 'A'; // var para caracteres entre aspas simples
+    bool verdadeiroefalso = true; // var para declarar variaveis do tipo booleano, true or false
+
+    printf("valor de x: %d\n", x);
+    printf("valor de pi: %f\n", pi);
+    printf("letra: %c\n", letra);
+    printf("1 verdadeiro e 0 falso: %d\n", verdadeiroefalso);
+
+    return 0;
+}
