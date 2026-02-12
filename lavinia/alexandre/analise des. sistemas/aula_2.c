181f67fef5b4fc2313c43098116bde1ca73da830
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2026-02-12 11:33:41 -0300

    update

--- lavinia/alexandre/analise des. sistemas/aula_2.c
+++ lavinia/alexandre/analise des. sistemas/aula_2.c
@@ -9,10 +9,17 @@ int main (){
     char letra = 'A'; // var para caracteres entre aspas simples
     bool verdadeiroefalso = true; // var para declarar variaveis do tipo booleano, true or false
 
-    printf("valor de x: %d\n", x);
-    printf("valor de pi: %f\n", pi);
-    printf("letra: %c\n", letra);
-    printf("1 verdadeiro e 0 falso: %d\n", verdadeiroefalso);
+    printf("valor de x: %d\n", x); // %d imprime variaveis do tipo inteiro
+    printf("valor de pi: %f\n", pi); // %f imprime variaveis com 2 casas decimais
+    printf("letra: %c\n", letra); // %c imorime caracteres entre aspas
+    printf("1 verdadeiro e 0 falso: %d\n", verdadeiroefalso); // %d também usado para true or false 1 e 0
 
     return 0;
 }
+
+// exercício 1
+// a - declarar um tipo de cada das variveis e atribuir o nome que quiser
+// b - imprimir as variaveis
+// c - declarar tres variaveis do mesmo tipo em uma linha e imprimi-las uma ao lado da utra
+// d - declarar uma variavel em uma linha e atribuir o valor em outra e imprimi-la
+// e - escrever seu nome
