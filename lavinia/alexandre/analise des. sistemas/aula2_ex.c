b4ee5a439a559610a2c98aafe8bc1f2dbf20b75a
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2026-02-19 11:55:33 -0300

    update

--- lavinia/alexandre/analise des. sistemas/aula2_ex.c
+++ lavinia/alexandre/analise des. sistemas/aula2_ex.c
@@ -4,21 +4,23 @@
 // exercício 1
 // a - declarar um tipo de cada das variveis e atribuir o nome que quiser
 // b - imprimir as variaveis
-// c - declarar tres variaveis do mesmo tipo em uma linha e imprimi-las uma ao lado da utra
+// c - declarar tres variaveis do mesmo tipo em uma linha e imprimi-las uma ao lado da outra
 // d - declarar uma variavel em uma linha e atribuir o valor em outra e imprimi-la
 // e - escrever seu nome
 
 int main(){
-    int numero = 10;
-    float pi = 3.14;
-    char letra = 'L';
-    bool gato = true;
-    
-    printf("numero %d\n", numero);
-    printf("float %f\n", pi);
-    printf("melhor letra do alfabeto %c\n", letra);
-    printf("gato %d\n", gato);
+    int numero = 8, x = 6, l = 4;
+    float pi = 3.14159;
+    char letra = 'L', letra2 = 'a', letra3 = 'v', letra4 = 'i';
+    int kitty;
+    kitty = 2;
+    bool meow = true;
+
+    printf("numero %d,%d,%d\n", numero, x, l);
+    printf("float %.2f\n", pi);
+    printf("nome: %c,%c,%c,%c\n", letra, letra2, letra3, letra4);
+    printf("kitty: %d\n", kitty);
+    printf("gato %d\n", meow);
     
     return 0;
 }
-
