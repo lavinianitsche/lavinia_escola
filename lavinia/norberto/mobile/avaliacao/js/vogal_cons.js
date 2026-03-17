4f4b82f6e976308f3a59b4b4162eb69bce221b00
lavinianitsche <lavinia.nitsche@escola.pr.gov>
2026-03-17 07:18:06 -0300

    update

--- lavinia/norberto/mobile/avaliacao/js/vogal_cons.js
+++ lavinia/norberto/mobile/avaliacao/js/vogal_cons.js
@@ -5,8 +5,11 @@ function letra() {
     letra = document.getElementById("letra").value;
 
     if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
-        document.getElementById("result").innerHTML = "vogal";
-    } else  {
+        document.getElementById("result").innerHTML = "vogal minuscula";
+        
+    } else if (letra == "A" || letra == "E" || letra == "I" || letra == "O" || letra == "U") {
+        document.getElementById("result").innerHTML = "vogal maiuscula";
+    } else {
         document.getElementById("result").innerHTML = "consoante";
     }
 
