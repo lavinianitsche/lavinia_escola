53aaa4221a2a46e744963da10da03477a1fe2cff
lavinianitsche <lavinia.nitsche@escola.pr.gov>
2026-03-30 11:31:47 -0300

    update

--- lavinia/norberto/mobile/trab2/js/media_notas.js
+++ lavinia/norberto/mobile/trab2/js/media_notas.js
@@ -1,5 +1,8 @@
 function calcular() {
+    
+    let continuar = true;
 
+    
     let turma;
     let nome;
     let media;
@@ -8,24 +11,45 @@ function calcular() {
     let bi_tres;
     let bi_quatro;
 
+    
+
     turma = document.getElementById("turma").value;
     nome = document.getElementById("nome").value;
     bi_um = Number(document.getElementById("notau").value);
     bi_dois = Number(document.getElementById("notad").value);
     bi_tres = Number(document.getElementById("notat").value);
     bi_quatro = Number(document.getElementById("notaq").value);
-
+    
     media = (bi_um + bi_dois + bi_tres + bi_quatro) / 4
-
+        
+    let situacao = "";
+    
     if (media >= 60) {
-        document.getElementById("result").innerHTML = media + " aprovado";
+        situacao = "aprovado";
     } else {
-        document.getElementById("result").innerHTML = media + " reprovado";
+        situacao = "reprovado";
     }
-
+        
+    document.getElementById("result").innerHTML = "aluno: " + nome + " - media: " + media + " - " + situacao;
+    
     console.log(turma);
     console.log(nome);
     console.log(media);
+    console.log(situacao);
+
+    continuar = confirm("deseja calcular a média de outro aluno?");
+
+    if (continuar) {
+        document.getElementById("turma").value = "";
+        document.getElementById("nome").value = "";
+        document.getElementById("notau").value = "";
+        document.getElementById("notad").value = "";
+        document.getElementById("notat").value = "";
+        document.getElementById("notaq").value = "";
+        document.getElementById("result").innerHTML = "";
+    } else {
+        alert("calculo finalizado");
+    }
 
 }
 
