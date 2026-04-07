99f38de8e71a441201fc289385a300730f4c77ba
lavinianitsche <lavinia.nitsche@escola.pr.gov>
2026-04-07 08:17:14 -0300

    update

--- lavinia/norberto/mobile/media/js/media_notas.js
+++ lavinia/norberto/mobile/media/js/media_notas.js
@@ -1,14 +1,13 @@
 function calcular() {
     
-    // let turma;
-    // let nome;
+    let nome;
     let media;
     let tri_um;
     let tri_dois;
     let tri_tres;
-    
-    // turma = document.getElementById("turma").value;
-    // nome = document.getElementById("nome").value;
+
+    nome = document.getElementById("nome").value;
+    console.log("aluno: " + nome);
     tri_um = Number(document.getElementById("notau").value);
     console.log("1º tri: " + tri_um);
     tri_dois = Number(document.getElementById("notad").value);
@@ -16,7 +15,7 @@ function calcular() {
     tri_tres = Number(document.getElementById("notat").value);
     console.log("3º tri: " + tri_tres);
 
-    media = (tri_um +tri_dois +tri_tres) / 3
+    media = (tri_um + tri_dois + tri_tres) / 3
         
     let situacao = "";
     
@@ -26,12 +25,12 @@ function calcular() {
         situacao = "reprovado";
     }
         
-    // document.getElementById("result").innerHTML = "aluno: " + nome + " - media: " + media + " - " + situacao;
-    document.getElementById("result").innerHTML = "media final: " + media.toFixed(1) + " - " + situacao;
+    document.getElementById("result").innerHTML = "aluno: " + nome + " - media final: " + media.toFixed(0) + " - " + situacao;
+    // document.getElementById("result").innerHTML = "media final: " + media.toFixed(1) + " - " + situacao;
     
-    // console.log(turma);
-    // console.log(nome);
-    console.log("media final: " + media.toFixed(1));
+    console.log(" ");
+    console.log("aluno: " + nome);
+    console.log("media final: " + media.toFixed(0));
     console.log(situacao);
 
 }
