5bae41040f28c9cc0fbe1b9d09b1d67c8a841a02
lavinianitsche <lavinia.nitsche@escola.pr.gov>
2026-04-30 08:49:33 -0300

    update

--- lavinia/alexandre/analise des. sistemas/forlist.c
+++ lavinia/alexandre/analise des. sistemas/forlist.c
@@ -2,16 +2,17 @@
 
 int main() {
     
-    /*printf("exercicio 1\n");
-    for (int num = 0; num <= 10; num ++)
-    {
-        printf("%d\n", num);
-    }*/
+    printf("exercicio 1\n");
+    int somar = 0;
+    for (int num = 0; num <= 10; num ++){
+        somar += num;
+    }
+    printf("%d\n", somar);
     
 
 
 
-    /*printf("\nexercicio 2\n");
+    printf("\nexercicio 2\n");
     int tabuada;
     printf("digite um numero: ");
     scanf("%d", &tabuada);
@@ -20,12 +21,12 @@ int main() {
     
     for (int n = 1; n <=10; n++){
         printf("%d * %d = %d\n", tabuada, n, tabuada * n);
-    }*/
+    }
     
 
 
 
-    /*printf("\nexercicio 3\n");
+    printf("\nexercicio 3\n");
     int intervalo;
 
     printf("escreva o intervalo desejado: ");
@@ -33,12 +34,12 @@ int main() {
 
     for (int numero = 100; numero >= 0; numero -= intervalo){
         printf("%d\n", numero);
-    }*/
+    }
 
 
 
 
-    /*printf("\nexercicio 4\n");
+    printf("\nexercicio 4\n");
     int number;
     int maior;
 
@@ -57,12 +58,12 @@ int main() {
         }
     }
     
-    printf("\n o maior valor digitado foi: %d\n", maior);*/
+    printf("\n o maior valor digitado foi: %d\n", maior);
     
 
 
 
-    /*printf("\nexercicio 5\n");
+    printf("\nexercicio 5\n");
     int idade;
     int velho = 0;
     
@@ -75,12 +76,12 @@ int main() {
         }
     }
             
-    printf("pessoas maiores de 18 anos: %d", velho);*/
+    printf("pessoas maiores de 18 anos: %d", velho);
             
 
 
 
-    /*printf("\nexercicio 6\n");
+    printf("\nexercicio 6\n");
     int patinhos;
     
     printf("digite quantos patinhos a mamae patinha tem: ");
@@ -97,7 +98,7 @@ int main() {
 
         } else {
 
-            pprintf("%d patinhos foram passear\n", p);
+            printf("%d patinhos foram passear\n", p);
             printf("alem das montanhas para brincar\n");
             printf("a mamae gritou: qua qua qua qua\n");
             printf("mas so nenhum patinho voltou de la\n");
@@ -108,18 +109,64 @@ int main() {
     printf("a mamae patinha foi procurar\n");
     printf("alem das montanhas, na beira do mar\n");
     printf("a mamae gritou: qua qua qua qua\n");
-    printf("e os %d patinhos voltaram de la\n", patinhos);*/
+    printf("e os %d patinhos voltaram de la\n", patinhos);
     
 
 
     
-    printf("\nexercicio 6\n");
+    printf("\nexercicio 7\n");
+    int numbero;
+    int pares = 0;
+    int impares = 0;
+    
+    for (int il = 0; il < 10; il++) {
+        printf("digite um numero: ", il + 1);
+        scanf("%d", &numbero);
+        
+        if (numbero % 2 == 0) {
+            pares++;
+        } else {
+            impares++;
+        }
+    }
+    
+    printf("\nquantidade de numeros pares: %d\n", pares);
+    printf("quantidade de numeros impares: %d\n", impares);
+
+
+
+    printf("\nexercicio 8\n");
+    int n1 = 0, num1 = 0, num2 = 1, proxnum;
+    printf("digite o numero de termos: ");
+    scanf("%d", &n1);
+    
+    for(int in = 0; in < n1; in++){
+        printf("%d ", num1);
+        
+        proxnum = num1 + num2;
+        num1 = num2;
+        num2 = proxnum;
+    }
+    printf("(%d termos)\n", n1);
 
 
 
 
+    printf("\nexercicio 9\n");
+    float nota;
+    float soma = 0;
+    float media;
 
-
+    for (int me = 1; me <= 10; me++) {
+        printf("Digite a nota do %d aluno: ", me);
+        scanf("%f", &nota);
+        
+        soma = soma + nota;
+    }
+    
+    media = soma / 10;
+    
+    printf("\nmedia da turma: %.2f\n", media);
 
     return 0;
 }
